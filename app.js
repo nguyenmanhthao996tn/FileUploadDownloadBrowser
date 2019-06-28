52c1f7fbdfcef237bbb7ae12215d9e4fd3211512
nguyenmanhthao996tn <manhthao.spm@gmail.com>
2019-06-27 21:57:44 -0700

    Update: Add upload file feature on homepage

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const { window } = new JSDOM(`...`);
 const { document } = (new JSDOM(`...`)).window;
 
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const app = express();
 const port = 3000;
 const contentsPath = __dirname + '\\contents\\';
 
 const path = require('path');
 
+app.use(fileUpload());
 app.use('/download', express.static(path.join(__dirname, 'contents')));
 
 app.get('/', (req, res) => {
@@ -47,6 +49,22 @@ app.get('/', (req, res) => {
                 }
             });
         }
+    });
+});
+
+app.post('/upload', function (req, res) {
+    if (Object.keys(req.files).length == 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    let sampleFile = req.files.sampleFile;
+
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv(path.join(__dirname, '/uploads/', sampleFile.name), function (err) {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.sendFile(path.join(__dirname, '/uploaded.html'));
     });
 });
 
